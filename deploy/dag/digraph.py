02bcf6524f1bb7405523160ded06acc804397cb4
Michael Luckeneder <michael@luckeneder.net>
2013-03-28 21:22:30 -0400

    modified some files

--- deploy/dag/digraph.py
+++ deploy/dag/digraph.py
@@ -1,23 +1,24 @@
-"""Represents a directed graph""" 
+"""Represents a directed graph"""
 # Represents a directed graph
 #
 # Copyright (c) 2013 by Michael Luckeneder
 #
 
-from dag.edge import Edge
-from dag.vertex import Vertex
 from collections import OrderedDict
+
 
 class DirectedGraph(object):
     """Represents a directed graph (DAG)"""
     def __init__(self):
         """Initialize DAG"""
         self.edges = OrderedDict()
-        self.vertices = list()
-    
+        self.vertices = set()
+
     def add_edge(self, edge):
         """Add an edge to DAG"""
         self.edges[edge.begin] = edge
+        self.vertices.add(edge.begin)
+        self.vertices.add(edge.end)
 
     def calculate_least_cost(self):
         """Calculate the total cost of the graph"""
--- deploy/dag/edge.py
+++ deploy/dag/edge.py
@@ -1,11 +1,17 @@
-# 
+#
 # Represents an edge in a DAG
 #
 # Copyright (c) 2013 by Michael Luckeneder
 #
 
+
 class Edge(object):
     """Represents an edge in a DAG"""
     def __init__(self, begin, end):
-    	"""Initialize edge"""
+        """Initialize edge"""
         self.begin, self.end = begin, end
+        self.weight = 0
+
+    def __str__(self):
+        """Returns String representation of edge"""
+        return "(%s,%s,%i)" % (self.begin, self.end, self.weight)
